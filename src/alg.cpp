856e1741c9ce734285284127d98bc02a58de5c85
annChekh <anua.chekhonadskikh@mail.ru>
2025-04-05 01:38:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,19 +1,24 @@
 // Copyright 2021 NNTU-CS
 #include <cstdint>
 #include <algorithm>
+
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int a = 0; a < len; a++) {
         for (int b = a + 1; b < len; b++) {
-            if (arr[a] + arr[b] == value) count++;
+            if (arr[a] + arr[b] == value) {
+                count++;
+            }
         }
     }
     return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
     int count = 0;
     int left = 0;
     int right = len - 1;
+
     while (left < right) {
         int sum = arr[left] + arr[right];
         if (sum == value) {
@@ -30,8 +35,10 @@ int countPairs2(int *arr, int len, int value) {
     }
     return count;
 }
+
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
+
     for (int i = 0; i < len; i++) {
         int target = value - arr[i];
         int left = i + 1;
@@ -42,12 +49,12 @@ int countPairs3(int *arr, int len, int value) {
             if (arr[mid] == target) {
                 count++;
                 int j = mid - 1;
-                while (j > i && arr[j] == target) {
+                while (j >= left && arr[j] == target) {
                     count++;
                     j--;
                 }
                 j = mid + 1;
-                while (j < len && arr[j] == target) {
+                while (j <= right && arr[j] == target) {
                     count++;
                     j++;
                 }
