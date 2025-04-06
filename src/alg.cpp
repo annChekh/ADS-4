e1989ac48924914197370c8a26d014fb00670c95
annChekh <anua.chekhonadskikh@mail.ru>
2025-04-06 14:25:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,6 @@
 // Copyright 2021 NNTU-CS
 #include <cstdint>
 #include <algorithm>
-
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int a = 0; a < len; a++) {
@@ -14,60 +13,68 @@ int countPairs1(int *arr, int len, int value) {
     return count;
 }
 
-int countPairs2(int *arr, int len, int value) {
-    int count = 0;
-    int left = 0;
-    int right = len - 1;
-
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            count++;
-            left++;
-            right--;
-            while (left < right && arr[left] == arr[left - 1]) left++;
-            while (left < right && arr[right] == arr[right + 1]) right--;
-        } else if (sum < value) {
-            left++;
-        } else {
-            right--;
+int kPairs2(int *arr, int len, int value) {
+  int k = 0;
+  for (int i = 0; i < len; i++) {
+    int lEl = i + 1;
+    int rEl = len - 1;
+    while (lEl <= rEl) {
+      int centre = lEl + (rEl - lEl) / 2;
+      if (arr[i] + arr[centre] == value) {
+        k++;
+        int l = centre - 1;
+        while (l >= lEl && arr[i] + arr[l] == value) {
+          k++;
+          l--;
         }
+        int r = centre + 1;
+        while (r <= rEl && arr[i] + arr[r] == value) {
+          k++;
+          r++;
+        }
+        break;
+      } else if (arr[i] + arr[centre] < value) {
+        lEl = centre + 1;
+      } else {
+        rEl = centre - 1;
+      }
     }
-    return count;
+  }
+  return k;
 }
 
-int countPairs3(int *arr, int len, int value) {
-    int count = 0;
-
-    for (int i = 0; i < len; i++) {
-        int target = value - arr[i];
-        int left = i + 1;
-        int right = len - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == target) {
-                count++;
-                int j = mid - 1;
-                while (j >= left && arr[j] == target) {
-                    count++;
-                    j--;
-                }
-                j = mid + 1;
-                while (j <= right && arr[j] == target) {
-                    count++;
-                    j++;
-                }
-                break;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+int kPairs3(int *arr, int len, int value) {
+  int k = 0;
+  int lEl = 0;
+  int rEl = len - 1;
+  while (lEl < rEl) {
+    int summa = arr[lEl] + arr[rEl];
+    if (summa == value) {
+      if (arr[lEl] != arr[rEl]) {
+        int lEl_val = arr[lEl];
+        int rEl_val = arr[rEl];
+        int lEl_k = 1;
+        int rEl_k = 1;
+        while (lEl + 1 < rEl && arr[lEl + 1] == lEl_val) {
+          lEl++;
+          lEl_k++;
         }
-        while (i + 1 < len && arr[i] == arr[i + 1]) {
-            i++;
+        while (rEl - 1 > lEl && arr[rEl - 1] == rEl_val) {
+          rEl--;
+          rEl_k++;
         }
+        k += lEl_k * rEl_k;
+        lEl++;
+        rEl--;
+      } else {
+        k += (rEl - lEl + 1) * (rEl - lEl) / 2;
+        break;
+      }
+    } else if (summa < value) {
+      lEl++;
+    } else {
+      rEl--;
     }
-    return count;
+  }
+  return k;
 }
